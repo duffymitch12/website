b9a456cf0998b3578196afa43dbb12dd9afa30ec
duffymit <duffymit@umich.eduexit>
2022-06-24 13:24:41 -0400

    fixed index.js

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const { appendFileSync } = require('fs');
 const app = express();
 const path = require('path');
 
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')))
 
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views')) 
+app.set('views', path.join(__dirname, '/views'))
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -22,5 +23,4 @@ app.get('/hobbies', (req, res) => {
     res.render('hobbies');
 })
 
-const port = process.env.PORT || 3000;
 
